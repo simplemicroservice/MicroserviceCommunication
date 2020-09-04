94b5b853b0c7f768e5f0572adf60e18a16cd601c
papamidnyt <msg2saumya@gmail.com>
2020-09-04 17:23:03 +0530

    Using Reactive Web to get Movie information from the MovieAPI using WebClient.Builder bean

--- Movie-Catalog-API/src/main/java/io/saumya/controller/Controller.java
+++ Movie-Catalog-API/src/main/java/io/saumya/controller/Controller.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import io.saumya.model.CatalogItem;
 import io.saumya.model.Movie;
@@ -22,32 +23,36 @@ public class Controller {
 	@Autowired
 	private RestTemplate restTemplate;
 	
+	@Autowired
+	private WebClient.Builder webClientBuilder;
+	
 	@RequestMapping("/{userId}")
+	//this method getMovieCatalog returning a list of movie with userRating according to UserId
 	public List<CatalogItem> getMovieCatalog(@PathVariable("userId") String userId){
 		
-		//for now we hard coded the UserRatingAPI inside our controller
-		//RestTemplate restTemplate = new RestTemplate();
-		//Movie movie=restTemplate.getForObject("localhost:8082/movies/matrix", Movie.class);
-		//step-1  get all rated movieId
-			/*For now i am going to hardcode the rating and movieId from Uer-API
-			 * so i copy the model UserRating from the User-API*/
+		//WebClient.Builder builder =WebClient.builder();
+		
 		List<UserRating> ratings = Arrays.asList(new UserRating("1234", 4),
 												 new UserRating("5678",3)
 												);
 		//step-2  for each movieId call movieAPI and get Details
 		return ratings.stream().map(rating->{
-			Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
-			return new CatalogItem(movie.getName(),"Description: Desc",rating.getRating());
-		}).collect(Collectors.toList());
+			//Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
+			Movie movie = webClientBuilder.build()
+				.get()
+				.uri("http://localhost:8082/movies/"+rating.getMovieId())
+				.retrieve()
+				.bodyToMono(Movie.class)
+				.block();
+			
+			return new CatalogItem(
+					               movie.getName(),
+					               "Description: Desc",
+					               rating.getRating());}
+								  ).collect(Collectors.toList());
 		
 		//step 3- put all them together
-		//take from 11 video starting 12
 		
-		
-		
-		//return Collections.singletonList(
-			//	new CatalogItem("name:Titanic","Desc:Romantic",5)
-			//	);
 	}
 	
 }
