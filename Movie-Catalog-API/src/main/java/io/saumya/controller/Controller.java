a6cb42b4ed8e812493919c0951dcb1cd76b9fccd
papamidnyt <msg2saumya@gmail.com>
2020-09-07 22:04:37 +0530

    adding ratings uri

--- Movie-Catalog-API/src/main/java/io/saumya/controller/Controller.java
+++ Movie-Catalog-API/src/main/java/io/saumya/controller/Controller.java
@@ -13,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import io.saumya.model.CatalogItem;
+import io.saumya.model.ListOfUsersRating;
 import io.saumya.model.Movie;
 import io.saumya.model.UserRating;
 
@@ -28,31 +29,10 @@ public class Controller {
 	
 	@RequestMapping("/{userId}")
 	
-	public List<CatalogItem> getMovieCatalog(@PathVariable("userId") String userId){
-		
-		//WebClient.Builder builder =WebClient.builder();
-		
-		List<UserRating> ratings = Arrays.asList(new UserRating("1234", 4),
-												 new UserRating("5678",3)
-												);
-		//step-2  for each movieId call movieAPI and get Details
-		return ratings.stream().map(rating->{
-			//Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
-			Movie movie = webClientBuilder.build()
-				.get()
-				.uri("http://localhost:8082/movies/"+rating.getMovieId())
-				.retrieve()
-				.bodyToMono(Movie.class)
-				.block();
-			
-			return new CatalogItem(
-					               movie.getName(),
-					               "Description: Desc",
-					               rating.getRating());}
-								  ).collect(Collectors.toList());
-		
-		//step 3- put all them together
-		
+	public ListOfUsersRating getMovieCatalog(@PathVariable("userId") String userId){
+		//ListOfUsersRating listOfUserRating = restTemplate.getForObject("http://localhost:8083/userrating/users/"+userId, responseType)
+		ListOfUsersRating userRating =  restTemplate.getForObject("http://localhost:8083/userrating/users/"+userId, ListOfUsersRating.class);
+		return userRating;
 	}
 	
 }
